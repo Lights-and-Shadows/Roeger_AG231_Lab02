cd7e2cf29ba6b068c40dc6a632258c8cc4530b24
Jamie R <36210321+Lights-and-Shadows@users.noreply.github.com>
2020-09-13 14:05:26 -0400

    Made a working image target

---
+++ Roeger_AR_Assignment_2/Assets/Scripts/ToggleObj.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class ToggleObj : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject toggleBtn, knot;
+
+    public bool isToggled;
+    // Start is called before the first frame update
+    
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        if (isToggled)
+            isToggled = false;
+        else
+            isToggled = true;
+    }
+
+    void Start()
+    {
+        toggleBtn = GameObject.Find("btnToggleKnot");
+        toggleBtn.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+
+        isToggled = false;
+    }
+
+    void Update()
+    {
+        if (isToggled)
+            knot.SetActive(true);
+        else
+            knot.SetActive(false);
+    }
+}
